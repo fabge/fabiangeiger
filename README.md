7039898ff9490b4068d29878f2e376c5a45e36ec
fabge <geiger.fabian@outlook.com>
2019-10-04 15:54:42 +0200

    rewrite with tailwindcss

--- README.md
+++
@@ -1,3 +0,0 @@
-# fabiangeiger.com
-
-Personal website.
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'serif': ['Yrsa', "Georgia", "Cambria", "Times New Roman", "Times", "serif"],
+      'display': ['Oswald', "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+    },
+    extend: {
+    }
+  },
+  variants: {},
+  plugins: []
+}
