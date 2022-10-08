a35c30b2c35a6dc6ad5571e7d5d115caa10ecf9c
Fabian <fa.geiger@enbw.com>
2022-10-08 21:09:02 +0200

    Update

--- pelicanconf.py
+++ pelicanconf.py
@@ -22,10 +22,9 @@ PLUGIN_PATHS = ['plugins']
 PLUGINS = ['gallery']
 
 GALLERY_PATH = 'images/gallery'
-DIRECT_TEMPLATES = ['index', 'archives', 'articles']
+DIRECT_TEMPLATES = ['index', 'articles']
 
 PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = '{slug}/index.html'
-ARTICLES_SAVE_AS = 'articles/index.html'
 
 DEFAULT_DATE_FORMAT = '%b %d, %Y'
